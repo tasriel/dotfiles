1b958c6acd0e6d19ba88a28e651472ff4ab9b710
Jannis Icken <icken.jannis@gmail.com>
2025-10-30 07:32:49 +0100

    fix config indentation

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,19 +3,19 @@ local wezterm = require("wezterm")
 config = wezterm.config_builder()
 
 config = {
-    automatically_reload_config = true,
-    enable_tab_bar = false,
-    window_close_confirmation = "NeverPrompt",
-    window_decorations = "RESIZE", -- disable the title bar but enable the reisizable border
-    color_scheme = "Tokyo Night",
-    font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
-    font_size = 12.5,
-    window_padding = {
-        left = 3,
-        right = 3,
-        top = 0,
-        bottom = 0
-    }
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE", -- disable the title bar but enable the reisizable border
+	color_scheme = "Tokyo Night",
+	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+	font_size = 12.5,
+	window_padding = {
+		left = 3,
+		right = 3,
+		top = 0,
+		bottom = 0,
+	},
 }
 
 return config
