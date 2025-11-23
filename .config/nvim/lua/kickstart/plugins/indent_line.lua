737b864b31f8f2e897e36bfbfe8f916204709331
Jannis Icken <icken.jannis@gmail.com>
2025-11-23 14:26:20 +0100

    nvim: change style of indent-line

--- .config/nvim/lua/kickstart/plugins/indent_line.lua
+++ .config/nvim/lua/kickstart/plugins/indent_line.lua
@@ -1,11 +1,28 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {
-      indent = { char = '▏' },
-    },
-  },
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+
+  config = function()
+    local hooks = require 'ibl.hooks'
+
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, 'IblIndent', { fg = '#232433', nocombine = true })
+      vim.api.nvim_set_hl(0, 'IblActiveScope', { fg = '#363b54', nocombine = true })
+    end)
+
+    require('ibl').setup {
+      indent = {
+        highlight = {
+          'IblIndent'
+        },
+        char = '▏',
+        -- char = '▎',
+      },
+      scope = {
+        enabled = true,
+        highlight = "IblActiveScope",
+        char = '▎',
+      }
+    }
+  end,
 }
