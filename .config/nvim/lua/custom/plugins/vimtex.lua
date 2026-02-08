10071551e4fe664541e34c7aaaefcbd0c660b4e9
Jannis Icken <icken.jannis@gmail.com>
2026-02-08 18:43:18 +0100

    feat(nvim): vimtex use default leader key (instead of ',')

--- .config/nvim/lua/custom/plugins/vimtex.lua
+++ .config/nvim/lua/custom/plugins/vimtex.lua
@@ -6,7 +6,6 @@ return {
     init = function()
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_method = 'zathura'
-      vim.g.vimtex_mappings_prefix = ','
 
       vim.g.vimtex_compiler_latexmk = {
         out_dir = 'build',
