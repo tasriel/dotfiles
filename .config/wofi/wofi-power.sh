59b00e97f9c98d1530ac87b9c08a6cc12485f0f7
Jannis Icken <icken.jannis@gmail.com>
2026-04-17 08:52:35 +0200

    fix(i3): power menu

--- .config/wofi/wofi-power.sh
+++ .config/wofi/wofi-power.sh
@@ -9,8 +9,8 @@ Suspend_command="systemctl suspend"
 Back_command=""
 
 # you can customise the rofi command all you want ...
-rofi_command="rofi -theme /home/marco/.config/wofi/launcherSmoll.rasi"
-options=$'Back\nShutdown\nLogout\nReboot\nHibernate\nSuspend' 
+rofi_command="rofi -theme $HOME/.config/wofi/launcherSmoll.rasi"
+options='Back\nShutdown\nLogout\nReboot\nHibernate\nSuspend' 
 
 # ... because the essential options (-dmenu and -p) are added here
 eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
