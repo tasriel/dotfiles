a1501c91e1b80a925750ae269b27f39c316ff8ac
Jannis Icken <icken.jannis@gmail.com>
2026-04-20 09:19:48 +0200

    chore: setup script add dunst, feh, sunwait, wallpaper cronjob

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -72,5 +72,18 @@ cd .. && rm -rf polybar-themes
 # install rofi
 sudo apt install rofi
 
-# wallpaper - add this as cronjob to change wallpaper dynamically:
+# install dunst notification daemon
+sudo apt install dunst
+
+# wallpaper 
+sudo apt install feh
+
+# install sunwait for dynamic wallpaper
+cd /opt
+sudo git clone https://github.com/risacher/sunwait.git
+cd sunwait
+sudo make
+sudo cp sunwait /usr/local/bin/
+
+# add this as cronjob to change wallpaper dynamically:
 # 0 * * * * bash $HOME/dotfiles/scripts/set_wallpaper.bash
