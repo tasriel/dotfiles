beef1f9a01449640f1e94e38ec3496effd98587c
Jannis Icken <icken.jannis@gmail.com>
2026-05-02 09:27:18 +0200

    feat(nvim): nvim-colorizer also display RRGGBBAA-colors

--- .config/nvim/lua/custom/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/custom/plugins/nvim-colorizer.lua
@@ -2,7 +2,22 @@ return {
     {
         'norcalli/nvim-colorizer.lua',
         config = function ()
-            require('colorizer').setup()
+            require('colorizer').setup({
+                -- filetype specific options
+                '*';
+            }, {
+                -- default options
+                RGB      = true;         -- #RGB hex codes
+                RRGGBB   = true;         -- #RRGGBB hex codes
+                names    = true;         -- "Name" codes like Blue
+                RRGGBBAA = true;        -- #RRGGBBAA hex codes
+                rgb_fn   = false;        -- CSS rgb() and rgba() functions
+                hsl_fn   = false;        -- CSS hsl() and hsla() functions
+                css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn   = false;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
+                -- Available modes: foreground, background
+                mode     = 'background'; -- Set the display mode.
+            })
         end
     }
 }
