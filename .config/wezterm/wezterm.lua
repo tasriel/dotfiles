8a5d79055bbfc759f14235886b141f3119f03feb
Jannis Icken <icken.jannis@gmail.com>
2026-05-15 10:44:47 +0200

    feat(wezterm): add font to display polybar-icons

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,20 +2,21 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config = {
-	automatically_reload_config = true,
-	enable_tab_bar = false,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE", -- disable the title bar but enable the reisizable border
-	color_scheme = "Tokyo Night",
-	font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
-	font_size = 12.5,
-	window_padding = {
-		left = 3,
-		right = 3,
-		top = 0,
-		bottom = 0,
-	},
+config.automatically_reload_config = true
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE" -- disable the title bar but enable the reisizable border
+config.color_scheme = "Tokyo Night"
+config.font = wezterm.font_with_fallback({
+	{family = "JetBrainsMono Nerd Font", weight = "DemiBold" },
+	{family = "feather"},
+})
+config.font_size = 12.5
+config.window_padding = {
+	left = 3,
+	right = 3,
+	top = 0,
+	bottom = 0,
 }
 
 return config
