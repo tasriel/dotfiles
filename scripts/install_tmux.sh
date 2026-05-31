369226e41ba359e8bd553c19a3c80b671f987cec
Jannis Icken <icken.jannis@gmail.com>
2026-05-31 12:35:03 +0200

    feat(tmux): new theme

---
+++ scripts/install_tmux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# tokyo night theme (https://github.com/janoamaral/tokyo-night-tmux)
+sudo apt install bash bc coreutils gawk git jq playerctl
+tmux source $HOME/.config/tmux/tmux.conf
