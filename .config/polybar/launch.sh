3b53264b48bf0c707f23d18703bb8bfa18d1ec91
Jannis Icken <icken.jannis@gmail.com>
2026-05-31 11:40:58 +0200

    fix(polybar): setup for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,72 +10,21 @@ launch_bar() {
 	# Wait until the processes have been shut down
 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-	# Launch the bar
-	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
-		polybar -q top -c "$dir/$style/config.ini" &
-		polybar -q bottom -c "$dir/$style/config.ini" &
-	elif [[ "$style" == "pwidgets" ]]; then
-		bash "$dir"/pwidgets/launch.sh --main
-	else
-		polybar -q main -c "$dir/$style/config.ini" &	
-	fi
+	# Launch the bar on every monitor connected
+	for m in $(xrandr --query | grep " connected" | awk '{print $1}'); do
+        MONITOR=$m polybar --reload main -c "$dir/$style/config.ini" &
+    done
 }
 
-if [[ "$1" == "--material" ]]; then
-	style="material"
-	launch_bar
 
-elif [[ "$1" == "--shades" ]]; then
-	style="shades"
-	launch_bar
-
-elif [[ "$1" == "--hack" ]]; then
-	style="hack"
-	launch_bar
-
-elif [[ "$1" == "--docky" ]]; then
-	style="docky"
-	launch_bar
-
-elif [[ "$1" == "--cuts" ]]; then
-	style="cuts"
-	launch_bar
-
-elif [[ "$1" == "--shapes" ]]; then
-	style="shapes"
-	launch_bar
-
-elif [[ "$1" == "--grayblocks" ]]; then
+if [[ "$1" == "--grayblocks" ]]; then
 	style="grayblocks"
-	launch_bar
-
-elif [[ "$1" == "--blocks" ]]; then
-	style="blocks"
-	launch_bar
-
-elif [[ "$1" == "--colorblocks" ]]; then
-	style="colorblocks"
-	launch_bar
-
-elif [[ "$1" == "--forest" ]]; then
-	style="forest"
-	launch_bar
-
-elif [[ "$1" == "--pwidgets" ]]; then
-	style="pwidgets"
-	launch_bar
-
-elif [[ "$1" == "--panels" ]]; then
-	style="panels"
 	launch_bar
 
 else
 	cat <<- EOF
 	Usage : launch.sh --theme
 		
-	Available Themes :
-	--blocks    --colorblocks    --cuts      --docky
-	--forest    --grayblocks     --hack      --material
-	--panels    --pwidgets       --shades    --shapes
+	Available Theme: --grayblocks
 	EOF
 fi
