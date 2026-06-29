519d35f18efdb81bc06f597965d89a9e3f6e1cb5
Jannis Icken <icken.jannis@gmail.com>
2026-06-29 22:01:24 +0200

    chore(tmux): enhance setup script

--- scripts/install_tmux.sh
+++ scripts/install_tmux.sh
@@ -2,4 +2,12 @@
 
 # tokyo night theme (https://github.com/janoamaral/tokyo-night-tmux)
 sudo apt install bash bc coreutils gawk git jq playerctl
+
+# clone tpm repo
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install plugins
+~/.tmux/plugins/tpm/bindings/install_plugins
+
+# reload tmux config
 tmux source $HOME/.config/tmux/tmux.conf
