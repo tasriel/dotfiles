d792fa539ad7a61fb317b6cafc67ac63a081b539
Jannis Icken <icken.jannis@gmail.com>
2026-07-01 20:01:12 +0200

    chore(nvim): remove unused code for nvim-ufo plugin

--- .config/nvim/lua/custom/plugins/nvim-ufo.lua
+++ .config/nvim/lua/custom/plugins/nvim-ufo.lua
@@ -9,15 +9,8 @@ return {
       end,
     },
     init = function()
-      -- Neovim Einstellungen für besseres Folding (wichtig für ufo)
       vim.o.foldcolumn = '1' -- Zeigt eine kleine Spalte links für Folds an
-      vim.o.foldlevel = 99   -- Öffnet Dateien standardmäßig ungeklappt
-      vim.o.foldlevelstart = 99
       vim.o.foldenable = true
-
-      -- Keymaps zum Öffnen/Schließen aller Folds auf einmal
-      vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = "Alle Folds öffnen" })
-      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = "Alle Folds schließen" })
     end,
   },
 }
