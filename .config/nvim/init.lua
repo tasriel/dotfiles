00beebbf2a25f8be6e9a1bacf288df1588aa6a65
Jannis Icken <icken.jannis@gmail.com>
2026-07-13 15:08:13 +0200

    feat(nvim): add sqlfluff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -810,12 +810,18 @@ require('lazy').setup({
       -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        sql = { 'sqlfluff' }
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
+      formatters = {
+        sqlfluff = {
+          expect_errors = true,
+        },
+      }
     },
   },
 
