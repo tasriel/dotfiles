9471865a70dffef23b3f581700531a2e2e02fad3
Jannis Icken <icken.jannis@gmail.com>
2026-09-21 13:47:37 +0200

    fix(nvim): use fork of nvim-colorizer plugin

--- .config/nvim/lua/custom/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/custom/plugins/nvim-colorizer.lua
@@ -1,23 +1,21 @@
 return {
-    {
-        'norcalli/nvim-colorizer.lua',
-        config = function ()
-            require('colorizer').setup({
-                -- filetype specific options
-                '*';
-            }, {
-                -- default options
-                RGB      = true;         -- #RGB hex codes
-                RRGGBB   = true;         -- #RRGGBB hex codes
-                names    = true;         -- "Name" codes like Blue
-                RRGGBBAA = true;        -- #RRGGBBAA hex codes
-                rgb_fn   = false;        -- CSS rgb() and rgba() functions
-                hsl_fn   = false;        -- CSS hsl() and hsla() functions
-                css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-                css_fn   = false;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
-                -- Available modes: foreground, background
-                mode     = 'background'; -- Set the display mode.
-            })
-        end
-    }
+  {
+    'catgoose/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup {
+        filetypes = { '*' },
+        user_default_options = {
+          RGB = true, -- #RGB hex codes
+          RRGGBB = true, -- #RRGGBB hex codes
+          names = true, -- "Name" codes like Blue
+          RRGGBBAA = true, -- #RRGGBBAA hex codes
+          rgb_fn = false, -- CSS rgb() and rgba() functions
+          hsl_fn = false, -- CSS hsl() and hsla() functions
+          css = false, -- Enable all CSS features
+          css_fn = false, -- Enable all CSS *functions*
+          mode = 'background',
+        },
+      }
+    end,
+  },
 }
